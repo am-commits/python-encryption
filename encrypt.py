a940abcff02766afbdab88d981eb2dbb89ea131a
Andreas Michailidis <michandreas001@gmail.com>
2022-04-25 20:32:35 +0300

    encryption fully implemented

--- encrypt.py
+++ encrypt.py
@@ -15,24 +15,28 @@ def create_key(id):
 def encrypt_file(file, id):
   f = open(file)
   file_contents = f.read()
-  data_key = create_key(id)
-
-  filen = Fernet(data_key)
-  file_contents_encrypted = filen.encrypt(file_contents)
+  data_key, data_key_plaintext = create_key(id)
+  filen = Fernet(data_key_plaintext)  
+  file_contents_encrypted = filen.encrypt(bytes((file_contents), 'utf-8'))
+  f = open(file, "w")
+  f.write(str(file_contents_encrypted))
 
 def decrypt_key(encrypted_key):
     kms_client = boto3.client('kms')
     response = kms_client.decrypt(CiphertextBlob=encrypted_key)
-    return base64.b64encode('b' + (response['Plaintext']))
+    return base64.b64encode((response['Plaintext']))
 
 def decrypt_file(file, encrypted_key):
-    file_contents = file.read()
+    f = open(file)
+    file_contents = f.read()
     data_key = decrypt_key(encrypted_key)
     filen = Fernet(data_key)
-    file_contents_decrypted = filen.decrypt(file_contents)
+    file_contents_decrypted = filen.decrypt(bytes((file_contents), 'utf-8'))
 
-    file.write(file_contents_decrypted)
+    f.write(file_contents_decrypted)
 
 os.chdir("C:\\Users\\andre\\Documents\\Python personal\\python-encryption")
 create_key('arn:aws:kms:eu-central-1:329080927726:key/0fc53ee3-ea51-4afe-aa34-170dfc351bcf')
-decrypt_file(encrypt_file('test.txt','arn:aws:kms:eu-central-1:329080927726:key/0fc53ee3-ea51-4afe-aa34-170dfc351bcf'))
+encrypt_file("test.txt", 'arn:aws:kms:eu-central-1:329080927726:key/0fc53ee3-ea51-4afe-aa34-170dfc351bcf' )
+decrypt_key(create_key('arn:aws:kms:eu-central-1:329080927726:key/0fc53ee3-ea51-4afe-aa34-170dfc351bcf')[0])
+# decrypt_file('test.txt', create_key('arn:aws:kms:eu-central-1:329080927726:key/0fc53ee3-ea51-4afe-aa34-170dfc351bcf')[0])
